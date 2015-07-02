e67a826e56c409a6558cde96e80862327ffdaa4f
Philipp Wagner <bytefish@gmx.de>
2015-07-02 21:02:52 +0200

    Add Logging
    Debug Messages to enhance Logging.

--- ConnectR/ConnectR/Hubs/ChatHub.cs
+++ ConnectR/ConnectR/Hubs/ChatHub.cs
@@ -1,12 +1,17 @@
 ﻿using ConnectR.Services;
+using log4net;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 using System;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace ConnectR.Hubs
 {
     public class ChatHub : Hub
     {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        
         private readonly IChatService chatService;
 
         public ChatHub(IChatService chatService)
@@ -16,6 +21,33 @@ namespace ConnectR.Hubs
                 throw new ArgumentNullException("chatService");
             }
             this.chatService = chatService;
+        }
+
+        public override Task OnConnected()
+        {
+            if (log.IsDebugEnabled)
+            {
+                log.DebugFormat("OnConnected (ConnectionId = {0})", Context.ConnectionId);
+            }
+            return base.OnConnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            if (log.IsDebugEnabled)
+            {
+                log.DebugFormat("OnReconnected (ConnectionId = {0})", Context.ConnectionId);
+            }
+            return base.OnReconnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            if(log.IsDebugEnabled)
+            {
+                log.DebugFormat("OnDisconnected (ConnectionId = {0}, StopCalled = {1})", Context.ConnectionId, stopCalled);
+            }
+            return base.OnDisconnected(stopCalled);
         }
 
         public void Send(string name, string message)
